9b0b8c0164936b5cc8acf8414f7f32e14593f5e2
Miroslav Hrubeš <mira.hrubes@seznam.cz>
2021-11-24 14:43:24 +0100

    7dni_verze_1

--- ukol_2.py
+++ ukol_2.py
@@ -1,1 +1,25 @@
-print("ahoj zkouška opět tady zase")
+import csv
+with open ('vstup.csv', encoding="utf-8") as csvfile:
+    reader = csv.reader(csvfile, delimiter = ",")
+    sedmdeni = 0
+    datumcitac = 0
+    prutokcitac = 1
+    prutok = 0
+    for row in reader:
+        try:
+            datum = str(row[0]+","+row[1]+","+row[2]+","+row[3]+","+row[4]+",")
+            
+            prutok = float(row[5])
+            sedmdeni = sedmdeni + prutok
+            if (datumcitac % 7) == 0:
+                datum7 = datum
+            if (prutokcitac % 7) == 0:
+                sedmdenistr = str(sedmdeni)
+                sedmdeni = 0
+                print(datum7+sedmdenistr)
+        except ValueError:
+            prutok = 0    
+        finally:
+            datumcitac += 1
+            prutokcitac += 1    
+  
