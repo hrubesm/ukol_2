91729f3b9a8d1e88df0577282b8fc630c9b6ab9f
Miroslav Hrubeš <mira.hrubes@seznam.cz>
2021-11-25 14:57:40 +0100

    rok_pokus

--- ukol_2.py
+++ ukol_2.py
@@ -6,14 +6,18 @@ with open ('vstup.csv', encoding="utf-8") as csvfile,\
     writer_dny = csv.writer(csvoutfile1)
     writer_rok = csv.writer(csvoutfile2)
 
+    mesic1 = 12
+    rok1 = 0
+    rocni = 0
     sedmdeni = 0
     datumcitac = 0
+    rocnicitac = 1
     prutokcitac = 1
     prutok = 0
     for row in reader:
         try:
             week = row
-            datum = (row[0]+","+row[1]+","+row[2]+","+row[3]+","+row[4])            
+            datum = str(row[0]+","+row[1]+","+row[2]+","+row[3]+","+row[4])            
             prutok = float(row[5])
             sedmdeni = sedmdeni + prutok
             if (datumcitac % 7) == 0:
@@ -25,16 +29,32 @@ with open ('vstup.csv', encoding="utf-8") as csvfile,\
                 writer_dny.writerow(week)
                 
                 #print(datum7,sedmdenistr)
+            
+            year = row
+            rocni = rocni + prutok
+            rok2 = float(row[2])
+            mesic2 = float(row[3])
+            if rok1 < rok2:
+                datum365 = datum
+                rok1 = rok2
+            if (rok1 < rok2) and (rok1 != 0):
+                rocnistr = str(f"{(rocni-prutok/rocnicitac):.4f}")
+                rocni = 0
+                rocnicitac = 0
+                mesic1 = mesic2
+                year = (datum365, rocnistr)
+                writer_rok.writerow(year)
 
         except ValueError:
             prutok = 0    
         finally:
             datumcitac += 1
-            prutokcitac += 1    
+            prutokcitac += 1 
+            rocnicitac += 1   
 
     
-    posledni = str(sedmdeni/(datumcitac%7))
-    week = (datum7, posledni)
+    posledniden = str(sedmdeni/(datumcitac%7))
+    week = (datum7, posledniden)
     writer_dny.writerow(week)
     #print(datum7, posledni)
 
